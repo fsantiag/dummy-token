758c2695584474f8af7242850284be21d3302b7f
Felipe Santiago <jfelipe.sfilho@gmail.com>
2021-12-26 14:44:59 +0100

    remove white spaces

--- src/Dummy.sol
+++ src/Dummy.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.6;
 // use latest solidity version at time of writing, need not worry about overflow and underflow
 
-/// @title ERC20 Contract 
+/// @title ERC20 Contract
 
 contract Token {
 
@@ -24,14 +24,14 @@ contract Token {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-        totalSupply = _totalSupply; 
+        totalSupply = _totalSupply;
         balanceOf[msg.sender] = totalSupply;
     }
 
     /// @notice transfer amount of tokens to an address
     /// @param _to receiver of token
     /// @param _value amount value of token to send
-    /// @return success as true, for transfer 
+    /// @return success as true, for transfer
     function transfer(address _to, uint256 _value) external returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
         _transfer(msg.sender, _to, _value);
@@ -43,20 +43,20 @@ contract Token {
     /// @param _to receiver of token
     /// @param _value amount value of token to send
     // Internal function transfer can only be called by this contract
-    //  Emit Transfer Event event 
+    //  Emit Transfer Event event
     function _transfer(address _from, address _to, uint256 _value) internal {
-        // Ensure sending is to valid address! 0x0 address cane be used to burn() 
+        // Ensure sending is to valid address! 0x0 address cane be used to burn()
         require(_to != address(0));
         balanceOf[_from] = balanceOf[_from] - (_value);
         balanceOf[_to] = balanceOf[_to] + (_value);
         emit Transfer(_from, _to, _value);
     }
 
-    /// @notice Approve other to spend on your behalf eg an exchange 
+    /// @notice Approve other to spend on your behalf eg an exchange
     /// @param _spender allowed to spend and a max amount allowed to spend
     /// @param _value amount value of token to send
     /// @return true, success once address approved
-    //  Emit the Approval event  
+    //  Emit the Approval event
     // Allow _spender to spend up to _value on your behalf
     function approve(address _spender, uint256 _value) external returns (bool) {
         require(_spender != address(0));
@@ -65,12 +65,12 @@ contract Token {
         return true;
     }
 
-    /// @notice transfer by approved person from original address of an amount within approved limit 
+    /// @notice transfer by approved person from original address of an amount within approved limit
     /// @param _from, address sending to and the amount to send
     /// @param _to receiver of token
     /// @param _value amount value of token to send
     /// @dev internal helper transfer function with required safety checks
-    /// @return true, success once transfered from original account    
+    /// @return true, success once transfered from original account
     // Allow _spender to spend up to _value on your behalf
     function transferFrom(address _from, address _to, uint256 _value) external returns (bool) {
         require(_value <= balanceOf[_from]);
